0e357bc94f863a8ed57e202455f0b629d57072b4
fasilpk09 <fasilpk09@gmail.com>
2017-02-17 19:12:17 +0000

    Add files via upload

---
+++ face.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="styles.css" rel="stylesheet">
+	<title>FaceBook Login</title>
+	<style type=text/css >
+	.hr {
+	font-size:36px;color:white;font-family:sans-serif;background:#3B5998;
+	}
+	.main{
+  	margin-top: 65px;border: 5px;border-color:#3B5998;border-style: solid;padding-top: 60px;padding-left: 60px;padding-bottom: 60px;padding-right: 60px;
+  		 }
+  		 .logo{padding-top: 100px;padding-left: 60px;padding-bottom: 60px;padding-right: 60px;}
+  .right{
+  	align-self: right;
+  }
+	</style>
+</head>
+<body>
+
+<div class="hr">
+<table width="100%">
+<tr>
+<td>
+<img src="img/logo.png" width="220px"> 
+</td>
+<td  align="right">
+<form>
+<input type="text" name="Username" placeholder="Username">
+<input type="pasSsword" name="pass" placeholder="Password">
+<input type="Submit" value="Sign In">
+<br>
+<font class="logo" size="3px">Forgot Password?</font>
+</form>
+</td>
+</tr>
+</table>
+</div><br><br>
+
+<table width="100%">
+<tr><td style="padding-left: 18px">
+
+<font size="6px" face="sans-serif"><b>Connect with Friends and World<br> Around You on facebook</b></font>
+<br><br>
+<img src="img/connect.png"><br>
+<img src="img/fblogo.png">&nbsp;&nbsp;See photos and updates from friends in news feed<br><br>
+	<img src="img/fblogo.png">&nbsp;&nbsp;Share what`s new in your life on your time line<br><br>
+	<img src="img/fblogo.png">&nbsp;&nbsp;Find more of what you are looking for with graph search<br><br></font>
+</td>
+<td>
+<div align="right">
+
+<br>
+<form>
+<table cellspacing="15px" cellpadding="4px"><font size="68px">
+	<tr><td><font style="font-family: sans-serif;size: 20px">Sign Up</font></td></tr>
+	<tr><td><input type="text" name="fn" placeholder="First Name">&nbsp;&nbsp;&nbsp;<input type="text" name="ln" placeholder="Last Name"></td></tr>
+	<tr><td colspan="2"><input size="47px" type="text" name="Email" placeholder="Email"></td></tr>
+	<tr><td><input type="password" name="pass1" placeholder="Enter Password">&nbsp;&nbsp;&nbsp;<input type="password" name="pass2" placeholder="Confirm Password"></td>
+	</tr>
+	<tr><td><input type="radio" name="Gender">Male<input type="radio" name="Gender">Female</td></tr>
+	<tr><td><font face ="verdana" style="size: 1px">By clicking SignUp you agree to our terms and that you<br> have read our data use policy,including our cookie use.</td></tr>
+	<tr><td><img src="img/signup.png"></td></tr>
+
+	</table>
+	</font>
+	</form>
+	</td>
+	</table>
+
+</div>
---
+++ test.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test</title>
+</head>
+  <body>
+  <style type="text/css">
+  	
+  .head{
+  	font-size: 12px;color: green;background-color: grey;
+  }
+  .mail{
+  	font-size: 10px;font-family: sans-serif;color: purple;
+  }
+  .main{
+  	margin-top: 65px;border: 5px;border-color:#3B5998;border-style: solid;padding-top: 60px;padding-left: 60px;padding-bottom: 60px;padding-right: 60px;
+  }
+  
+  </style>
+<div class =container>
+<h1 class="main" align="center" p border=><font face ="sans-serif">Welcome To baabtra.com</font></h1>
+<h1>Facebook</h1>
+<img src="resources/t5.png" height="134" width="134">
+</div>
+<br>
+<uol class="container">
+<li>Car</li>
+<li>cup</li>
+<li>coffee</li>
+</uol>
+<ol class="container">
+<li>Car</li>
+<li>cup</li>
+<li>coffee</li>
+</uol>
+<h4><span style="color:red">ReallyImp</span></h4>
+<h5><font face ="sans-serif">Facebook</font></h5>
+<div class="container">
+<table class="medium"  cellpadding="1" cellspacing="4" border="3">
+	<tr class="head"><th >Name</th><th>E-mail</th><th>Number</th></tr>
+	<tr>
+		<td>Fazil P.K</td>
+		<td class="mail">fazilpk09@gmail.com</td>
+		<td>9447800418</td>
+
+	</tr>
+	    <td>Anonyms</td>
+		<td class="mail">anon@gmail.com</td>
+		<td>9447434352</td>
+		
+	</tr>
+	<tr>
+		<td>Vishnu</td>
+		<td class="mail">vishnu@gmail.com</td>
+		<td>9447843233</td>
+		
+	</tr>
+
+</table>
+</div><br>
+<form>
+	<input type="text" name="textin" action="POST" placeholder="First Name">
+	<input type="submit">
+
+</form>
+<p style=color:red;margin-right:30px>Hello</p>
+</body>
+</html>
